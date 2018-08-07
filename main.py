a41ecf7fc4155c24e2ab8c436108c67c0ba0069c
JamielC <jcapatayan@csumb.edu>
2018-08-07 22:38:22 +0000

    changed alot of things like alot so ye

--- main.py
+++ main.py
@@ -9,9 +9,28 @@ the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
-    
-    
-    
+
+
+user_info={
+    'name':"",
+    'sign':"",
+    'url':""
+    }
+
+zodiac_backgrounds={
+    'aquarius':'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/BKpZoQ4viqlda90c/videoblocks-zodiac-sign-aquarius-and-horoscope-wheel-on-the-dark-blue-background_by6wer0hg_thumbnail-full01.png',
+    'pisces':'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/BKpZoQ4viqlda90c/videoblocks-zodiac-sign-pisces-and-horoscope-wheel-on-the-dark-blue-background_s3zb_kirz_thumbnail-full01.png',
+    'aries':'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/BKpZoQ4viqlda90c/videoblocks-zodiac-sign-aries-and-horoscope-wheel-on-the-dark-blue-background_snzz6l_prz_thumbnail-full01.png',
+    'taures':'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/BKpZoQ4viqlda90c/videoblocks-zodiac-sign-taurus-and-horoscope-wheel-on-the-dark-blue-background_h3d4r5khm_thumbnail-full01.png',
+    'leo':'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/BKpZoQ4viqlda90c/videoblocks-zodiac-sign-leo-and-horoscope-wheel-on-the-dark-blue-background_shfn95ybg_thumbnail-full01.png',
+    'capricorn':'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/BKpZoQ4viqlda90c/videoblocks-zodiac-sign-capricorn-and-horoscope-wheel-on-the-dark-blue-background_s3eo5vjobf_thumbnail-full01.png',
+    'gemini':'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/BKpZoQ4viqlda90c/videoblocks-zodiac-sign-gemini-and-horoscope-wheel-on-the-dark-blue-background_h3xhlc_abf_thumbnail-full01.png',
+    'libra':'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/BKpZoQ4viqlda90c/videoblocks-zodiac-sign-libra-and-horoscope-wheel-on-the-dark-blue-background_htrywarsm_thumbnail-full01.png',
+    'scorpio':'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/BKpZoQ4viqlda90c/videoblocks-zodiac-sign-scorpio-and-horoscope-wheel-on-the-dark-blue-background_hhghwyibg_thumbnail-full01.png',
+    'virgo':'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/BKpZoQ4viqlda90c/videoblocks-zodiac-sign-virgo-and-horoscope-wheel-on-the-dark-blue-background_bntos9trg_thumbnail-full01.png',
+    'sagittarius':'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/BKpZoQ4viqlda90c/videoblocks-zodiac-sign-sagittarius-and-horoscope-wheel-on-the-dark-blue-background_htvwq0rbm_thumbnail-full01.png',
+    'cancer':'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/BKpZoQ4viqlda90c/videoblocks-zodiac-sign-cancer-and-horoscope-wheel-on-the-dark-blue-background_s3wxt2uphf_thumbnail-full01.png'
+}
     
 class Mainpage(webapp2.RedirectHandler):
     def get(self):
@@ -19,24 +38,22 @@ class Mainpage(webapp2.RedirectHandler):
         self.response.write(main_template.render())
     
     def post(self):
-        main_template= the_jinja_env.get_template('templates/main_page.html')
+        main_template= the_jinja_env.get_template('templates/main_page2.html')
         name = self.request.get('user_name')
-        user_info={
-            'name':name
-        }
+        print(name)
+        user_info['name']=name
         self.response.write(main_template.render(user_info))
         
 
 class Mainpage2(webapp2.RedirectHandler):
     def get(self):
         main_template= the_jinja_env.get_template('templates/main_page2.html')
-        self.response.write(main_template.render())
+        self.response.write(main_template.render(user_info))
     def post(self):
-        main_template= the_jinja_env.get_template('templates/main_page2.html')
-        name = self.request.get('user_name')
-        user_info={
-            'name':name
-        }
+        main_template= the_jinja_env.get_template('templates/results.html')
+        sign = self.request.get('sign')
+        user_info['sign']= sign
+        user_info['url']= zodiac_backgrounds[sign]
         self.response.write(main_template.render(user_info))    
 
 
@@ -47,7 +64,7 @@ class Mainpage2(webapp2.RedirectHandler):
 class Aquarius(webapp2.RedirectHandler): #1
     def get(self):
         aqua_template= the_jinja_env.get_template('templates/aquarius.html')
-        self.response.write(aqua_template.render())
+        self.response.write(aqua_template.render(user_info))
 
 
 class Pisces(webapp2.RedirectHandler): #2
